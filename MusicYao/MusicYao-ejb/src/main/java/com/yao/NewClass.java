58a77d220e131f257a133f01dce934b2b9d91f3a
世宁 <@yao-thinkpad>
2013-10-28 20:38:23 +0800

    Init maven project

---
+++ MusicYao/MusicYao-ejb/src/main/java/com/yao/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.yao;
+
+/**
+ *
+ * @author 世宁
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("asdads");
+    }
+}
